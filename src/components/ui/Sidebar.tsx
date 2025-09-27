7a984e229b3dbd0ff80d2d3beb56626ae27231dc
Dewa Putra Hernanda <dewananda089@gmail.com>
2025-09-27 11:37:33 +0700

    feat: adjust sidebar

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -1,14 +1,37 @@
+"use client";
+
 import Menu from "@/config/data/menu";
 import Link from "next/link";
+import { usePathname, useSelectedLayoutSegment } from "next/navigation";
+import { Component } from "react";
 
 export const Sidebar = () => {
   const sidebar = Menu.sidebar();
+  const pathname = usePathname();
+
+  function isActive(path: string) {
+    return path === "/" ? pathname === "/" : pathname.startsWith(path);
+  }
+
   return (
-    <div className="w-[180px] bg-slate-900/80 shadow-lg shadow-blue-900/40 h-fit text-amber-50 rounded-2xl flex flex-col gap-2">
+    <div className="w-[180px] bg-slate-900/80 shadow-lg shadow-blue-900/40 h-fit text-amber-50 rounded-2xl flex flex-col gap-2 p-2">
       {sidebar.map((v, k) => {
+        const Icon = v.icon.name;
+        const active = isActive(v.href);
+
         return (
-          <Link href={v.href} key={k} className="text-center p-2">
-            {v.label}
+          <Link
+            href={v.href}
+            key={k}
+            className={`hover:!no-underline justify-center flex items-center gap-2 text-center w-full p-2 rounded-lg transition ${
+              active
+                ? "bg-slate-800 text-blue-400 font-semibold"
+                : "hover:bg-slate-800 hover:text-blue-300"
+            }`}
+          >
+            <span className="hidden sm:block">{v.label}</span>
+
+            <Icon size={v.icon.size} />
           </Link>
         );
       })}
--- src/config/data/menu.ts
+++ src/config/data/menu.ts
@@ -1,9 +1,30 @@
+import { MdHomeFilled } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
 export default class Menu {
   static sidebar() {
     return [
-      { label: "Home", href: "/" },
-      { label: "About", href: "/about" },
-      { label: "Contact", href: "/contact" },
+      {
+        label: "Home",
+        href: "/",
+        icon: {
+          name: MdHomeFilled,
+          size: 20,
+        },
+      },
+      {
+        label: "About",
+        href: "/about",
+        icon: {
+          name: FaUser,
+          size: 16,
+        },
+      },
+      {
+        label: "Contact",
+        href: "/contact",
+        icon: { name: MdEmail, size: 20 },
+      },
     ];
   }
 }
