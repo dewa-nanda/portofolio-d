d60dcf572e78a7562136be5d28548c4b6c95e0ad
Dewa Putra Hernanda <dewananda089@gmail.com>
2025-09-28 08:06:10 +0700

    feat: add note console

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,5 +1,34 @@
+"use client";
+
 import HomePage from "@/components/pages/HomePage";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    console.clear();
+
+    console.log(
+      "%c🚀 Welcome to My Portofolio fellas 🚀",
+      "color:#F7F4EA; font-size:16px; font-weight:bold;"
+    );
+
+    console.log(
+      `%c
+██╗  ██╗███████╗████████╗
+██║ ██╔╝██╔════╝╚══██╔══╝
+█████╔╝ █████╗     ██║   
+██╔═██╗ ██╔══╝     ██║   
+██║  ██╗███████╗   ██║   
+╚═╝  ╚═╝╚══════╝   ╚═╝   
+    `,
+      "color:#F7F4EA; font-weight:bold; font-size:12px;"
+    );
+
+    console.log(
+      "%c✨ Crafted with love by Dewa ✨",
+      "color:#F7F4EA; font-size:14px; font-style:italic;"
+    );
+  }, []);
+
   return <HomePage />;
 }
